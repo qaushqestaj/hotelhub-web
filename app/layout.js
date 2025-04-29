42ce54d1bf65cdb6737a686552e36801b944f9fc
Qaush Qestaj <qaushqestaj@hotmail.com>
2025-04-29 11:55:25 +0200

    first commit

---
+++ app/layout.js
@@ -0,0 +1,33 @@
+import { Josefin_Sans } from 'next/font/google';
+
+const josefin = Josefin_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+import '@/app/_styles/globals.css';
+import Header from './_components/Header';
+
+export const metadata = {
+  title: {
+    template: '%s | The Wild Oasis',
+    default: 'Welcome / The Wild Oasis',
+  },
+  description:
+    'Luxury cabin hotel, located in the heart of the Italian Dolomites',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+}
